c9a4e9aa2dfc5ac8a7bd3a84b96f3c0e64439d70
cheinfongsen <sungsung3201@gmail.com>
2016-06-19 00:46:51 +0800

    remove myHash part from ifs.h

--- src/IFS/ifs.cpp
+++ src/IFS/ifs.cpp
@@ -14,35 +14,35 @@ IFS::~IFS() {}
 
 void IFS::loadFault(const char *filename) {
 
-    ifstream faultFile(filename);
-    string line, temp;
+	ifstream faultFile(filename);
+	string line, temp;
 	FaultType faulttype;
 	char* ch1;
 	char* ch2;
-    while(getline(faultFile, line)) {
-        size_t start = 0, end = 0;
-        end = line.find_first_of(" \t\n", start);
-        temp = line.substr(start, end - start);
-        //cout << temp << '\t';
-        if (temp != "") _faultId.push_back(atoi(temp.c_str()));
-        start = end + 1;
-        
-        while((end = line.find_first_of(" \t\n", start)) != string::npos) {
-            temp = line.substr(start, end - start);
-            start = end + 1;
-            if(temp == "") continue;
-            else {
-                //cout << temp << '\t';
-                _faultGateId.push_back(atoi(temp.c_str()));
-                break;
-            }
-        }     
-        temp = line.substr(start, end - start);
+	while(getline(faultFile, line)) {
+		size_t start = 0, end = 0;
+		end = line.find_first_of(" \t\n", start);
+		temp = line.substr(start, end - start);
+		//cout << temp << '\t';
+		if (temp != "") _faultId.push_back(atoi(temp.c_str()));
+		start = end + 1;
+
+		while((end = line.find_first_of(" \t\n", start)) != string::npos) {
+			temp = line.substr(start, end - start);
+			start = end + 1;
+			if(temp == "") continue;
+			else {
+				//cout << temp << '\t';
+				_faultGateId.push_back(atoi(temp.c_str()));
+				break;
+			}
+		}     
+		temp = line.substr(start, end - start);
 		temp.erase(0,temp.find_first_not_of("\t"));
 		if(int(temp[temp.length()-1])==13)
 			temp.erase(temp.length()-1,1);
 		//cout << temp << '\n';
-        //if (temp != "") _faultTypeList.push_back(temp);
+		//if (temp != "") _faultTypeList.push_back(temp);
 		if(temp=="SA0")
 			faulttype=SA0;
 		else if(temp=="SA1")
@@ -65,26 +65,26 @@ void IFS::loadFault(const char *filename) {
 			faulttype=RDOB_NOT;
 		else if(temp=="RDOB_BUFF")
 			faulttype=RDOB_BUFF;
- 		//cout<<endl<<temp<<endl<<temp.length();
+		//cout<<endl<<temp<<endl<<temp.length();
 		_faultTypeList.push_back(faulttype);       
-    }
+	}
 }
 
 // find primary outputs of each fault gate
 void IFS::faultOut(Circuit& c) {
-    cout << "faultOut!\n";
-    for(size_t i = 0; i < _faultGateId.size(); i++) {
-        map<int, bool> out;
-        c.outputDFS(_faultGateId[i], out);
-        vector<int> outs;
-        cout << '[' << _faultId[i] << "]\t";
-        for(map<int, bool>::iterator it = out.begin(); it != out.end(); it++) {
-            cout << it->first << ' ';
-            outs.push_back(it->first);
-        }
-        cout << '\n';
-        _faultOutList.push_back(outs);
-    }
+	cout << "faultOut!\n";
+	for(size_t i = 0; i < _faultGateId.size(); i++) {
+		map<int, bool> out;
+		c.outputDFS(_faultGateId[i], out);
+		vector<int> outs;
+		cout << '[' << _faultId[i] << "]\t";
+		for(map<int, bool>::iterator it = out.begin(); it != out.end(); it++) {
+			cout << it->first << ' ';
+			outs.push_back(it->first);
+		}
+		cout << '\n';
+		_faultOutList.push_back(outs);
+	}
 }
 
 vector<int>* IFS::getfId(){
--- src/IFS/ifs.h
+++ src/IFS/ifs.h
@@ -10,36 +10,36 @@ class Circuit;
 class MyHash;
 
 enum FaultType {
-    SA0,
-    SA1,
-    NEG,
-    RDOB_AND,
-    RDOB_NAND,
-    RDOB_OR,
-    RDOB_NOR,
-    RDOB_XOR,
-    RDOB_NXOR,
-    RDOB_NOT,
-    RDOB_BUFF
+	SA0,
+	SA1,
+	NEG,
+	RDOB_AND,
+	RDOB_NAND,
+	RDOB_OR,
+	RDOB_NOR,
+	RDOB_XOR,
+	RDOB_NXOR,
+	RDOB_NOT,
+	RDOB_BUFF
 };
 
 class IFS {
- 
+
   public:
-    IFS();
-    ~IFS();
-    void loadFault(const char* filename);
-    void faultOut(Circuit&);
+	IFS();
+	~IFS();
+	void loadFault(const char* filename);
+	void faultOut(Circuit&);
 	vector<int>* getfId();				// get fault id
 	vector<int>* getfGId();				// get fault gate id
 	vector<FaultType>* getfTList();		// get fault type list
 	vector< vector<int> >* getfOList(); // get fault primary output list
-    
+
   private:
-    vector<int> _faultId;
-    vector<int> _faultGateId;
-    vector<FaultType> _faultTypeList;
-    vector< vector<int> > _faultOutList;
+	vector<int> _faultId;
+	vector<int> _faultGateId;
+	vector<FaultType> _faultTypeList;
+	vector< vector<int> > _faultOutList;
 };
 
 
